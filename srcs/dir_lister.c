6a0a806293587bfc7bd9166def26bbaee1a15271
Samuel Cordero <samuelcorderorodrigo@gmail.com>
2024-10-13 14:21:41 +0200

    initial test

--- srcs/dir_lister.c
+++ srcs/dir_lister.c
@@ -1,35 +1,79 @@
 #include "ft_ls.h"
 
+static int	ft_strncasecmp(const char *s1, const char *s2, size_t n)
+{
+    size_t  i;
+
+    i = 0;
+    if (n == 0)
+        return (0);
+
+    while (i < n && s1[i] != '\0' && s2[i] != '\0') {
+        unsigned char c1 = (unsigned char)ft_tolower(s1[i]);
+        unsigned char c2 = (unsigned char)ft_tolower(s2[i]);
+
+        if (c1 != c2)
+            return c1 - c2;
+        i++;
+    }
+
+    if (i < n) {
+        unsigned char c1 = (unsigned char)ft_tolower(s1[i]);
+        unsigned char c2 = (unsigned char)ft_tolower(s2[i]);
+        return c1 - c2;
+    }
+
+    return 0;
+}
+
 void    get_files_init(DIR **dir_ptr, struct dirent **directory, const char *path)
 {
     *dir_ptr = opendir(path);
     *directory = readdir(*dir_ptr);
 }
 
+static void sortFiles(char **file_list, int sb_time, int reverse_sort) {
+    int swapped;
+    if (sb_time) {
+        ft_printf("Work in progress, no t flag yet\n");
+    }    
+    // char * ascii bubble sort
+    do {
+        swapped = 0;
+        for (int i = 0; file_list[i + 1]; i++) {
+            if ((reverse_sort == 0 && ft_strncasecmp(file_list[i], file_list[i + 1], ft_strlen(file_list[i])) > 0) ||
+                (reverse_sort == 1 && ft_strncasecmp(file_list[i], file_list[i + 1], ft_strlen(file_list[i])) < 0)) { // Ascending order if reverse_sort == 0, descending if reverse_sort == 1
+                char *tmp = file_list[i];
+                file_list[i] = file_list[i + 1];
+                file_list[i + 1] = tmp;
+                swapped = 1;
+            }
+        }
+    } while (swapped);
+}
+
 void    listDir(const char *path, int flags)
 {
-    DIR                *dir_ptr;
-    struct dirent    *directory;
-    char            *file_list;
-    char            *tmp;
+    DIR             *dir_ptr;
+    struct dirent   *directory;
+    char            *file_list[4096];
 
     get_files_init(&dir_ptr, &directory, path);
-    file_list = ft_strdup("");
     if (!directory)
         return ;
-    while (directory)
+    ft_bzero(file_list, sizeof(file_list));
+    int i = 0;
+    while (directory && i < 4096)
     {
-        if (ft_strncmp(directory->d_name, ".", 1) || flags & __SHOWHIDDEN) {
-            tmp = ft_strjoin(file_list, directory->d_name);
-            free(file_list);
-            file_list = ft_strjoin(tmp, " ");
-            free(tmp);
+        if (ft_strncmp(directory->d_name, ".", 1) || flags & __SHOWHIDDEN) { //dont check hidden files unless flag -a active
+            file_list[i++] = ft_strdup(directory->d_name);
+            //ft_printf("%s: %d %d\n", directory->d_name, directory->d_reclen, directory->d_type);
         }
         directory = readdir(dir_ptr);
     }
-    tmp = ft_strtrim(file_list, " \n\t\r\v");
-    free(file_list);
+    sortFiles(file_list, flags & __SORTTIME, flags & __REVERSESORT); //sort files (by name by default)
+    for (i = 0; file_list[i]; ++i) {
+        ft_printf("%s\n", file_list[i]);
+    }
     closedir(dir_ptr);
-    ft_printf("%s\n", tmp);
-    free(tmp);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -4,10 +4,54 @@ void    freeAndExit(int exit_code) {
     exit(exit_code);
 }
 
+int parseArgs(int argc, char **argv, char **path_list) {
+    int flags = 0;
+    int ctr = 0;
+    int i;
+    for (i = 1; i < argc; ++i) {
+        if (argv[i][0] == '-') { //parse arg
+            int j;
+            for (j = 1; argv[i][j]; ++j) {
+                if (argv[i][j] == 'R')
+                    flags |= __RECURSIVE;
+                else if (argv[i][j] == 'a')
+                    flags |= __SHOWHIDDEN;
+                else if (argv[i][j] == 'l')
+                    flags |= __SHOWLIST;
+                else if (argv[i][j] == 't')
+                    flags |= __SORTTIME;
+                else if (argv[i][j] == 'r')
+                    flags |= __REVERSESORT;
+                else if (argv[i][j] == '-')
+                    break ;
+            }
+            if (argv[i][j] == '-'){
+                ++i;
+                break ;
+            }
+        } else {
+            path_list[ctr++] = ft_strdup(argv[i]);
+        }
+    }
+    while (argv[i]) {
+        path_list[ctr++] = ft_strdup(argv[i]);
+        ++i;
+    }
+    return flags;
+}
+
 int main(int argc, char **argv) {
     ft_printf("args: %d, exec name:%s\n", argc, argv[0]);
-    listDir(argv[1], 0);
-/*     ft_printf("showing hidden files:\n");
-    listDir(argv[1], __SHOWHIDDEN); */
+    
+    char    *path_list[4096];
+    ft_bzero(path_list, sizeof(path_list));
+
+    int flags = parseArgs(argc, argv, path_list);
+    //here path_list should be sorted (ascii by default, time if -t flag)
+    //duplicates are ignored and listed twice
+    for (int i = 0; path_list[i]; ++i) {
+        listDir(path_list[i], flags);
+    }
+    
     freeAndExit(0);
 }
